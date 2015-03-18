6a2aebf9579de508fc0c7c941d8444d304af654b
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-18 17:41:31 +0100

    adding descriptions for intersect() and uintersect()

--- src/Dormilich/ArrayInterface.php
+++ src/Dormilich/ArrayInterface.php
@@ -108,7 +108,37 @@ interface ArrayInterface
 	 * @throws Exception Failed to flip the array.
 	 */
 	public function flip();
+	/**
+	 * Returns an array containing all the values of the array that are 
+	 * present in all the arguments. Note that keys are preserved. 
+	 * 
+	 * @param mixed $input First array to compare against.
+	 * @param integer $mode Determines whether the comparison should be made 
+	 * 			on the array values, the array keys or on both using the 
+	 * 			ArrayInterface::USE_VALUE and ArrayInterface::USE_KEY constants.
+	 * @return ArrayObject Returns an array containing all the entries from 
+	 * 			the array that are present in all of the other arrays. 
+	 */
 	public function intersect($input, $mode = ArrayInterface::USE_VALUE);
+	/**
+	 * Computes the intersection of arrays by using callback function(s) for 
+	 * comparison. 
+	 * 
+	 * When using either USE_VALUE or USE_KEY only one compare function must 
+	 * be provided. If USE_VALUE|USE_KEY is used the first compare function 
+	 * compares the array values, the second the array keys. If the internal 
+	 * function should be used for comparison, set the appropriate callback 
+	 * parameter to NULL.
+	 * 
+	 * @param mixed $input The first array to compare against.
+	 * @param callable $val_compare_func 
+	 * @param callable $key_compare_func 
+	 * @param integer $mode Determines whether the comparison should be made 
+	 * 			on the array values, the array keys or on both using the 
+	 * 			ArrayInterface::USE_VALUE and ArrayInterface::USE_KEY constants.
+	 * @return ArrayObject Returns an array containing all the entries from 
+	 * 			the array that are present in all of the other arrays. 
+	 */
 	public function uintersect($input, $compare_func, $mode = ArrayInterface::USE_VALUE);
 	/**
 	 * Return all the keys or a subset of the keys of the array.
