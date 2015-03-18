9510b44854016cc0764761ab7a20604fed348938
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-18 10:27:17 +0100

    listing the array functions that should make it into the array object.

---
+++ src/Dormilich/ArrayInterface.php
@@ -0,0 +1,40 @@
+<?php
+// ArrayInterface.php
+
+namespace Dormilich;
+
+interface ArrayInterface
+{
+	const USE_VALUE = 1;
+	const USE_KEY   = 2;
+	public function changeKeyCase($case = \CASE_LOWER);
+	public function chunk($size, $preserve_keys = false);
+	public function contains($needle, $strict = false);
+	public function countValues();
+	public function diff($input, $mode = ArrayInterface::USE_VALUE);
+	public function udiff($input, $compare_func, $mode = ArrayInterface::USE_VALUE);
+	public function filter($callback); // flag
+	public function flip();
+	public function intersect($input, $mode = ArrayInterface::USE_VALUE);
+	public function uintersect($input, $compare_func, $mode = ArrayInterface::USE_VALUE);
+	public function keys(); // search_value, strict
+	public function map($callback);
+	public function merge($input);
+	public function pop();
+	public function rand($num = 1);
+	public function reduce($callback, $initial = NULL);
+	public function replace($input);
+	public function reverse($preserve_keys = false);
+	public function search($search_value, $strict = false);
+	public function shift();
+	public function shuffle();
+	public function slice ($offset, $length = NULL, $preserve_keys = false);
+	public function sort($sort_flags = \SORT_REGULAR, $mode = ArrayInterface::USE_VALUE, $preserve_keys = false);
+	public function rsort($sort_flags = \SORT_REGULAR, $mode = ArrayInterface::USE_VALUE, $preserve_keys = false);
+	public function usort($callback, $mode = ArrayInterface::USE_VALUE, $preserve_keys = false);
+	public function splice($offset); // length, replacement
+	public function unique ($sort_flags = \SORT_STRING);
+	public function unshift($input);
+	public function values();
+	public function walk ($callback, $userdata = NULL);
+}
