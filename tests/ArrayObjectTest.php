2547a0054bb373ead597901bb4a722f1249d98d4
Dr. Alexander Stranzky <astranzky@velia.net>
2015-03-20 15:53:51 +0100

    creating tests for push()

--- tests/ArrayObjectTest.php
+++ tests/ArrayObjectTest.php
@@ -338,6 +338,9 @@ class ArrayObjectTest extends PHPUnit_Framework_TestCase
 	### pop()
 	#######################################################
 
+	/**
+     * @depends testCountableInterface
+     */
 	public function testPopOnNumericArray()
 	{
 		$xao = new XArray([3, 7]);
@@ -347,6 +350,9 @@ class ArrayObjectTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals([3], $xao->getArrayCopy());
 	}
 
+	/**
+     * @depends testCountableInterface
+     */
 	public function testPopOnAssocArray()
 	{
 		$xao = new XArray(['bar', 'foo']);
@@ -356,11 +362,98 @@ class ArrayObjectTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(['bar'], $xao->getArrayCopy());
 	}
 
+	/**
+     * @depends testCountableInterface
+     */
 	public function testPopOnEmptyArray()
 	{
 		$xao = new XArray([]);
 		
 		$this->assertCount(0, $xao);
 		$this->assertNull($xao->pop());
+	}
+
+	### push()
+	#######################################################
+
+	public function testPushReturnsSameInstance()
+	{
+		$xao = new XArray(['foo']);
+		$obj = $xao->push('bar');
+		
+		$this->assertSame($xao, $obj);
+	}
+
+	/**
+     * @depends testCountableInterface
+     * @depends testArrayAccessInterface
+     */
+	public function testPushWithSingleElementOnNumericArray()
+	{
+		$xao = new XArray(['foo']);
+		$this->assertCount(1, $xao);
+		$xao->push(8);
+		
+		$this->assertCount(2, $xao);
+		$this->assertSame(8,  $xao[1]);
+	}
+
+	/**
+     * @depends testCountableInterface
+     * @depends testArrayAccessInterface
+     */
+	public function testPushWithSingleElementOnHigherIndexedNumericArray()
+	{
+		$xao = new XArray([5 => 'foo']);
+		$this->assertCount(1, $xao);
+		$xao->push(8);
+		
+		$this->assertCount(2, $xao);
+		$this->assertSame(8,  $xao[6]);
+	}
+
+	/**
+     * @depends testCountableInterface
+     * @depends testArrayAccessInterface
+     */
+	public function testPushWithMultipleElementsOnNumericArray()
+	{
+		$xao = new XArray(['foo']);
+		$this->assertCount(1, $xao);
+		$xao->push(8, 'x', true);
+		
+		$this->assertCount(5,   $xao);
+		$this->assertSame(8,    $xao[1]);
+		$this->assertSame('x',  $xao[2]);
+		$this->assertSame(true, $xao[3]);
+	}
+
+	/**
+     * @depends testCountableInterface
+     * @depends testArrayAccessInterface
+     */
+	public function testPushWithSingleElementOnAssocArray()
+	{
+		$xao = new XArray(['foo' => 'bar']);
+		$this->assertCount(1, $xao);
+		$xao->push(8);
+		
+		$this->assertCount(2, $xao);
+		$this->assertSame(8,  $xao[0]);
+	}
+	/**
+     * @depends testCountableInterface
+     * @depends testArrayAccessInterface
+     */
+	public function testPushWithMultipleElementsOnAssocArray()
+	{
+		$xao = new XArray(['foo' => 'bar']);
+		$this->assertCount(1, $xao);
+		$xao->push(8, 'x', true);
+		
+		$this->assertCount(5,   $xao);
+		$this->assertSame(8,    $xao[0]);
+		$this->assertSame('x',  $xao[1]);
+		$this->assertSame(true, $xao[2]);
 	}
 }
