cb36f04154a553394d05b165024ac95718eb33e5
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-20 02:12:02 +0100

    creating imlementation for changeKeyCase()

--- src/Dormilich/ArrayObject.php
+++ src/Dormilich/ArrayObject.php
@@ -6,4 +6,19 @@ namespace Dormilich;
 class ArrayObject extends \ArrayObject # implements ArrayInterface
 {
 	// using parent Constructor unmodified
+
+	public function changeKeyCase($case = \CASE_LOWER)
+	{
+		$flag = filter_var($case, \FILTER_VALIDATE_INT, [
+			'options' => [
+				'min_range' => 0, 
+				'max_range' => 1, 
+				'default'   => \CASE_LOWER, 
+			], 
+		]);
+
+		$array = array_change_key_case($this->getArrayCopy(), $flag);
+
+		return new static($array);
+	}
 }
