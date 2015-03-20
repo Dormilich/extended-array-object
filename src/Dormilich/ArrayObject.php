fbb6db19a3b585d0f866b2c27ff54332aad7f08d
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-20 02:17:00 +0100

    adding documentation for changeKeyCase()

--- src/Dormilich/ArrayObject.php
+++ src/Dormilich/ArrayObject.php
@@ -7,6 +7,16 @@ class ArrayObject extends \ArrayObject # implements ArrayInterface
 {
 	// using parent Constructor unmodified
 
+	/**
+	 * Returns an array with all keys from the array lowercased or uppercased. 
+	 * Numbered indices are left as is. 
+	 * 
+	 * If the case parameter is invalid then its default value (CASE_LOWER) 
+	 * will be used instead.
+	 * 
+	 * @param integer $case Either CASE_UPPER or CASE_LOWER (default).
+	 * @return ArrayObject Returns an array with its keys lower or uppercased.
+	 */
 	public function changeKeyCase($case = \CASE_LOWER)
 	{
 		$flag = filter_var($case, \FILTER_VALIDATE_INT, [
@@ -17,6 +27,7 @@ class ArrayObject extends \ArrayObject # implements ArrayInterface
 			], 
 		]);
 
+		// does not return FALSE if an array is provided
 		$array = array_change_key_case($this->getArrayCopy(), $flag);
 
 		return new static($array);
