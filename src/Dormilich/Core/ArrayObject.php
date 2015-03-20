eff90b1d86cffc210b28e4b5b40b0fe061c386fd
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-20 02:39:24 +0100

    adding documentation for contains()

--- src/Dormilich/Core/ArrayObject.php
+++ src/Dormilich/Core/ArrayObject.php
@@ -33,6 +33,15 @@ class ArrayObject extends \ArrayObject # implements ArrayInterface
 		return new static($array);
 	}
 
+	/**
+	 * Checks if a value exists in the array using loose comparison unless 
+	 * strict is set.
+	 * 
+	 * @param mixed $needle The searched value.
+	 * @param bool $strict If the parameter strict is set to TRUE then contains() 
+	 *          will also check the types of the needle in the array.
+	 * @return boolean Returns TRUE if needle is found in the array, FALSE otherwise. 
+	 */
 	public function contains($needle, $strict = false)
 	{
 		$flag  = filter_var($strict, \FILTER_VALIDATE_BOOLEAN);
