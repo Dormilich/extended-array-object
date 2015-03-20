8a1951d6b0a705b979df1205aee62d201fc08414
Dr. Alexander Stranzky <astranzky@velia.net>
2015-03-20 11:25:00 +0100

    adding documentation for flip()

--- src/Dormilich/Core/ArrayObject.php
+++ src/Dormilich/Core/ArrayObject.php
@@ -51,6 +51,15 @@ class ArrayObject extends \ArrayObject # implements ArrayInterface
 		return in_array($needle, $this->getArrayCopy(), $flag);
 	}
 
+	/**
+	 * Exchanges all keys with their associated values in the array.
+	 * 
+	 * If a value has several occurrences, the latest key will be used as its 
+	 * value, and all others will be lost. 
+	 * 
+	 * @return ArrayObject Returns the flipped array.
+	 * @throws RuntimeException Failed to flip the array.
+	 */
     public function flip()
     {
     	try {
