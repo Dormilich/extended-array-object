e6b1f88c87c5543dd7746264648f105d4a988d78
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-21 15:15:59 +0100

    creating tests for keys()

--- tests/ArrayObjectTest.php
+++ tests/ArrayObjectTest.php
@@ -555,6 +555,55 @@ class ArrayObjectTest extends PHPUnit_Framework_TestCase
 		$xao->join('-');
 	}
 
+	### keys()
+	#######################################################
+
+	public function testKeysReturnsArrayObject()
+	{
+		$xao = new XArray([1, 2, 3]);
+		$obj = $xao->keys();
+
+		$this->assertInstanceOf($this->classname, $obj);
+		$this->assertNotSame($xao, $obj);
+	}
+
+	public function testKeysReturnsKeyValues()
+	{
+		$expected = [0 => 'foo', 1 => 'x'];
+		$xao      = new XArray(['foo' => 'bar', 'x' => 'y']);
+		$obj      = $xao->keys();
+
+		$this->assertCount(2, $obj);
+		$this->assertEquals($expected, (array) $obj)
+	}
+
+	public function testKeysFilteredByEqualValue()
+	{
+		$expected = [0 => 'x'];
+		$xao      = new XArray(['foo' => 'bar', 'x' => 'y']);
+		$obj      = $xao->keys('y', false);
+
+		$this->assertEquals($expected, (array) $obj);
+	}
+
+	public function testKeysFilteredByEqualValueDefaultFlag()
+	{
+		$expected = [0 => 'x'];
+		$xao      = new XArray(['foo' => 'bar', 'x' => 'y']);
+		$obj      = $xao->keys('y');
+
+		$this->assertEquals($expected, (array) $obj);
+	}
+
+	public function testKeysFilteredByIdenticalValue()
+	{
+		$expected = [0 => 'bar'];
+		$xao      = new XArray(['foo' => true, 'bar' => 1, 'x' => '1']);
+		$obj      = $xao->keys(1, true);
+
+		$this->assertEquals($expected, (array) $obj);
+	}
+
 	### map()
 	#######################################################
 
