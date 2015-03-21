c922303471af0c64b9717cabfe41ec7f932c0c4d
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-21 15:39:31 +0100

    adding documentation for values()

--- src/Dormilich/Core/ArrayObject.php
+++ src/Dormilich/Core/ArrayObject.php
@@ -226,6 +226,11 @@ class ArrayObject extends \ArrayObject implements \JsonSerializable #, ArrayInte
 		return $this;
 	}
 
+	/**
+	 * Returns all the values from the array and indexes the array numerically. 
+	 * 
+	 * @return ArrayObject The array of the values. 
+	 */
 	public function values()
 	{
 		$values = array_values($this->getArrayCopy());
