c28346d27e5ee47e672ebfd6100c63cc50d9aa4d
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-21 15:28:27 +0100

    simpifying the ilementation code

--- src/Dormilich/Core/ArrayObject.php
+++ src/Dormilich/Core/ArrayObject.php
@@ -140,16 +140,21 @@ class ArrayObject extends \ArrayObject implements \JsonSerializable #, ArrayInte
 		}
 	}
 
+	/**
+	 * Return all the keys or a subset of the keys of the array.
+	 * 
+	 * @param mixed $search_value If specified, then only keys containing 
+	 * 			these values are returned. 
+	 * @param boolean $strict Determines if strict comparison (===) should be 
+	 * 			used during the search. 
+	 * @return ArrayObject Returns an array of all the (specified) keys in the array.  
+	 */
 	public function keys()
 	{
-		if (!func_num_args()) {
-			$keys = array_keys($this->getArrayCopy());
-		}
-		else {
-			$args = func_get_args();
-			array_unshift($args, $this->getArrayCopy());
-			$keys = call_user_func_array('array_keys', $args);
-		}
+		$args = func_get_args();
+		array_unshift($args, $this->getArrayCopy());
+		$keys = call_user_func_array('array_keys', $args);
+
 		return new static($keys);
 	}
 
