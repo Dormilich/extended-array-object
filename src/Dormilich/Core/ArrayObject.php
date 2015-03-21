0f50f83cf515b3c5ac1356d4584082645e3ddf9b
Dr. Alexander Stranzky <astranzky@velia.net>
2015-03-21 10:59:43 +0100

    adding documentation for map()

--- src/Dormilich/Core/ArrayObject.php
+++ src/Dormilich/Core/ArrayObject.php
@@ -140,6 +140,18 @@ class ArrayObject extends \ArrayObject implements \JsonSerializable #, ArrayInte
 		}
 	}
 
+	/**
+	 * Returns an array containing all the elements of the array after 
+	 * applying the callback function to each one.
+	 * 
+	 * @param callable $callback Callback function to run for each element 
+	 *          in each array. Receives the element’s value and key as parameters.
+	 * @param boolean $preserve_keys When set to TRUE keys will be preserved. 
+	 * 			Default is FALSE which will reindex the array numerically.
+	 * @return ArrayObject Returns an array containing all the elements of the 
+	 *          array after applying the callback function to each one. 
+	 * @throws LogicException Invalid callback definition given.
+	 */
 	public function map(callable $callback, $preserve_keys = false)
 	{
 		try {
