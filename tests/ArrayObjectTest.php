66a1bd196d9c46b36f03d96f19766fce759a47e2
Bertold von Dormilich <Dormilich@netscape.net>
2015-03-22 19:56:06 +0100

    creating tests for reverse()

--- tests/ArrayObjectTest.php
+++ tests/ArrayObjectTest.php
@@ -996,6 +996,45 @@ class ArrayObjectTest extends PHPUnit_Framework_TestCase
 		$this->assertContains($values[0], $obj);
 	}
 
+	### reverse()
+	#######################################################
+
+	public function testReverseReturnsArrayObject()
+	{
+		$xao = new XArray([1, 2, 3]);
+		$obj = $xao->reverse();
+
+		$this->assertInstanceOf($this->classname, $obj);
+		$this->assertNotSame($xao, $obj);
+	}
+
+	public function testReverseSuccess()
+	{
+		$expected = [3, 2, 1];
+		$xao      = new XArray([1 => 1, 8 => 2, 4 => 3]);
+		$obj      = $xao->reverse();
+
+		$this->assertEquals($expected, (array) $obj);
+	}
+
+	public function testReverseRetainsAssocKeys()
+	{
+		$expected = ['c' => 3, 'b' => 2, 'a' => 1];
+		$xao      = new XArray(['a' => 1, 'b' => 2, 'c' => 3]);
+		$obj      = $xao->reverse();
+
+		$this->assertEquals($expected, (array) $obj);
+	}
+
+	public function testReversePreserveNumericKeys()
+	{
+		$expected = [2 => 3, 1 => 2, 0 => 1];
+		$xao      = new XArray([1, 2, 3]);
+		$obj      = $xao->reverse(true);
+
+		$this->assertEquals($expected, (array) $obj);
+	}
+
 	### shift()
 	#######################################################
 
