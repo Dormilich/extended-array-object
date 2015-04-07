e5313e7f7cbeb6df03ef31c1d184a6a211572c73
Dr. Alexander Stranzky <astranzky@velia.net>
2015-04-07 18:59:20 +0200

    adding documentation to countValues()

--- src/Dormilich/Core/ArrayObject.php
+++ src/Dormilich/Core/ArrayObject.php
@@ -279,6 +279,14 @@ class ArrayObject extends \ArrayObject implements \JsonSerializable #, ArrayInte
 		return in_array($needle, $this->getArrayCopy(), $flag);
 	}
 
+	/**
+	 * Returns an array using the values of the array as keys and their 
+	 * frequency in the array as values. 
+	 * 
+	 * @return ArrayObject Returns an associative array of values from array 
+	 *          as keys and their count as value. 
+	 * @throws RuntimeException A value is not a string or integer.
+	 */
 	public function countValues()
 	{
 		try {
